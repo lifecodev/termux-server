8416338685080d541a20536e2af2325c138ce4f1
LifeCodeV <thedelermit@mail.ru>
2022-04-22 23:14:50 +0300

    some fix

--- test_wifi.js
+++ test_wifi.js
@@ -1,4 +1,4 @@
-const Wifi_Termux = require("./termux/wifi")
+const Wifi_Termux = require("./termux/wifi.js")
 var Wifi_Termux = new Wifi_Termux()
 Wifi_Termux.Scan()
 console.log(Wifi_Termux.getWifiInfo())
