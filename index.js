35968ece9ebf6e4d0c279c1f74c52f0316b7a15e
Turbo-Oxid <58607972+Turbo-Oxid@users.noreply.github.com>
2022-04-22 22:10:54 +0300

    add connect routes

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const wifi = require('node-wifi');
+const jsonParser = express.json();
+
+function connect(ssid, password){
+    console.log(password);
+    if(ssid==undefined||password==undefined) return ({status:0, error:"ssid or password not entered"});
+    wifi.connect({ssid: ssid, password: password}, () => {
+        console.log('Connected');
+        return ({status:1}); 
+      });   
+}
 
 wifi.init({
     iface: null // network interface, choose a random wifi interface if set to null
@@ -16,6 +26,15 @@ app.get("/api/scan", function(req, resolve){
         }
       });
         
+});
+
+app.get("/api/connect", function(req, resolve){
+    console.log(req);
+    resolve.send(connect(req.query.ssid, req.query.password)); 
+});
+
+app.post("/api/connect", jsonParser, function(req, resolve) {
+    resolve.send(connect(req.body.ssid, req.body.password));
 });
 
 app.listen(8080, function(){
